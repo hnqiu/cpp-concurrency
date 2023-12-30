25f50a584c960696282813a5fff9a7363ff27e2f
hnqiu <huanneng.qiu@gmail.com>
2023-12-31 00:04:13 +1100

    Add assignment 3 solution

---
+++ a3_livelock.cpp
@@ -0,0 +1,21 @@
+#include <mutex>
+#include <thread>
+
+int x{0};
+std::mutex m;
+
+void func() {
+    std::lock_guard<std::mutex> l{m};
+    while (x == 0) {
+        x = 1 - x;
+    }
+}
+
+int main()
+{
+    std::thread thr1{ func };
+    std::thread thr2{ func };
+
+    thr1.join();
+    thr2.join();
+}
